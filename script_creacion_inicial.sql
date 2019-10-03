d68b5e3996e4e0d3a43d4441d7210a6883af7571
Frilo <francocirillo3@gmail.com>
2019-10-02 23:18:31 -0300

    Creacion inicial tablas

---
+++ script_creacion_inicial.sql
@@ -0,0 +1,265 @@
+USE GD2C2019
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF EXISTS (
+		SELECT *
+		FROM sys.schemas
+		WHERE name = 'NUNCA_INJOIN'
+		)
+BEGIN
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'FacturaProveedor'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.FacturaProveedor
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'Compra'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.Compra
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'Oferta'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.Oferta
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'Proveedor'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.Proveedor
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'Rubro'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.Rubro
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'Oferta'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.Oferta
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'FacturaProveedor'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.FacturaProveedor
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'Entrega'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.Entrega
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'Compra'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.Compra
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'Carga'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.Carga
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'Tarjeta'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.Tarjeta
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'Cliente'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.Cliente
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'Usuario'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.Usuario
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'FuncionalidadPorRol'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.FuncionalidadPorRol
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'Rol'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.Rol
+
+	IF EXISTS (
+			SELECT *
+			FROM sys.tables
+			WHERE object_name(object_id) = 'Funcionalidad'
+				AND schema_name(schema_id) = 'NUNCA_INJOIN'
+			)
+		DROP TABLE NUNCA_INJOIN.Funcionalidad
+END
+GO
+
+DROP SCHEMA NUNCA_INJOIN
+GO
+
+BEGIN
+	EXEC ('create schema NUNCA_INJOIN authorization [gdCupon2019]')
+
+	PRINT 'Creado schema NUNCA_INJOIN'
+END
+GO
+
+/*
+	CREACIÓN DE TABLAS
+*/
+USE GD2C2019
+GO
+
+CREATE TABLE NUNCA_INJOIN.Funcionalidad ("funcionalidad_id" VARCHAR(50) PRIMARY KEY);
+
+CREATE TABLE NUNCA_INJOIN.Rol (
+	"rol_id" VARCHAR(50) PRIMARY KEY,
+	"baja_logica" CHAR(1) NOT NULL DEFAULT 'N' CHECK (baja_logica IN ('S', 'N'))
+	);
+
+CREATE TABLE NUNCA_INJOIN.FuncionalidadPorRol (
+	"funcionalidad_id" VARCHAR(50) REFERENCES NUNCA_INJOIN.Funcionalidad,
+	"rol_id" VARCHAR(50) REFERENCES NUNCA_INJOIN.Rol,
+	PRIMARY KEY (
+		rol_id,
+		funcionalidad_id
+		)
+	);
+
+CREATE TABLE NUNCA_INJOIN.Usuario (
+	"usuario_id" VARCHAR(50) PRIMARY KEY,
+	"rol_id" VARCHAR(50) REFERENCES NUNCA_INJOIN.Rol,
+	"contrasenia" VARBINARY(32) NOT NULL,
+	"intentos_fallidos" SMALLINT DEFAULT 0,
+	"baja_logica" CHAR(1) NOT NULL DEFAULT 'N' CHECK (baja_logica IN ('S', 'N'))
+	);
+
+CREATE TABLE NUNCA_INJOIN.Cliente (
+	"cliente_id" NUMERIC(9) identity PRIMARY KEY,
+	"usuario_id" VARCHAR(50) NOT NULL REFERENCES NUNCA_INJOIN.Usuario,
+	"nombre" NVARCHAR(255),
+	"apellido" NVARCHAR(255),
+	"dni" NUMERIC(18, 0),
+	"mail" NVARCHAR(255),
+	"telefono" NUMERIC(18, 0),
+	"domicilio" NVARCHAR(255),
+	"localidad" NVARCHAR(255),
+	"codigo_postal" NVARCHAR(8),
+	"fecha_nac" DATETIME,
+	"credito" NUMERIC(18, 2) NOT NULL DEFAULT 200
+	);
+
+CREATE TABLE NUNCA_INJOIN.Tarjeta (
+	"tarjeta_id" NUMERIC(9) identity PRIMARY KEY,
+	"cliente_id" NUMERIC(9) REFERENCES NUNCA_INJOIN.Cliente,
+	"duenio" NVARCHAR(255),
+	numero NUMERIC(19)
+	);
+
+CREATE TABLE NUNCA_INJOIN.Carga (
+	"carga_id" NUMERIC(9) identity PRIMARY KEY,
+	"cliente_id" NUMERIC(9) REFERENCES NUNCA_INJOIN.Cliente,
+	"tarjeta_id" NUMERIC(9) REFERENCES NUNCA_INJOIN.Tarjeta,
+	"fecha" DATETIME NOT NULL,
+	"tipo_pago" NVARCHAR(100),
+	"monto" NUMERIC(18, 2)
+	);
+
+CREATE TABLE NUNCA_INJOIN.Rubro (
+	"rubro_id" NUMERIC(9) identity PRIMARY KEY,
+	"nombre_rubro" NVARCHAR(100)
+	);
+
+CREATE TABLE NUNCA_INJOIN.Proveedor (
+	"proveedor_id" NUMERIC(9) identity PRIMARY KEY,
+	"rubro_id" NUMERIC(9) REFERENCES NUNCA_INJOIN.Rubro,
+	"usuario_id" VARCHAR(50) NOT NULL REFERENCES NUNCA_INJOIN.Usuario,
+	"razon_social" NVARCHAR(100),
+	"mail" NVARCHAR(255),
+	"telefono" NUMERIC(18, 0),
+	"domicilio" NVARCHAR(255),
+	"localidad" NVARCHAR(255),
+	"ciudad" NVARCHAR(255),
+	"codigo_postal" NVARCHAR(8),
+	"ciut" NVARCHAR(20),
+	"nombre_contacto" NVARCHAR(255)
+	);
+
+CREATE TABLE NUNCA_INJOIN.Oferta (
+	oferta_codigo NVARCHAR(50) PRIMARY KEY,
+	proveedor_id NUMERIC(9) REFERENCES NUNCA_INJOIN.Proveedor,
+	descripcion NVARCHAR(255),
+	fecha_publicacion DATETIME,
+	fecha_vencimiento DATETIME,
+	precio_oferta NUMERIC(18, 2),
+	precio_lista NUMERIC(18, 2),
+	cantidad_disponible NUMERIC(18, 0)
+	)
+
+CREATE TABLE NUNCA_INJOIN.Compra (
+	compra_id NUMERIC(9) identity PRIMARY KEY,
+	oferta_id NVARCHAR(50) REFERENCES NUNCA_INJOIN.Oferta,
+	cliente_compra_id NUMERIC(9) REFERENCES NUNCA_INJOIN.Cliente,
+	fecha_compra datetime,
+	cantidad_comprada numeric(18,0)
+	)
+
+CREATE TABLE NUNCA_INJOIN.FacturaProveedor (
+	factura_id NUMERIC(9) identity ,
+	factura_tipo char(1),
+	compra_id NUMERIC(9) references NUNCA_INJOIN.Compra,
+	proveedor_id NUMERIC(9) references NUNCA_INJOIN.Proveedor,
+	fecha datetime,
+	numero numeric(18,0),
+	importe numeric(26,2),
+	PRIMARY KEY(factura_id, factura_tipo)
+)
