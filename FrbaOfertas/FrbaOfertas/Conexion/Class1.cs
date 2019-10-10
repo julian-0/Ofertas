6fd9e58019081a22baf93107474871ba2f5676e8
Julian <jm.ord98@gmail.com>
2019-10-10 14:58:26 -0300

    Generalizo la ruta de la base de datos + cambios menores

---
+++ FrbaOfertas/FrbaOfertas/Conexion/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace FrbaOfertas.Conexion
+{
+    public class c_Conexion
+    {
+        SqlConnection conexion = new SqlConnection(@"Data Source=localhost\SQLSERVER2012;Initial Catalog=GD2C2019;Persist Security Info=True;User ID=gdCupon2019;Password=gd2019");
+
+        //Se conecta con la base de datos
+        public SqlConnection AbrirConexion()
+        {
+            if (conexion.State == ConnectionState.Closed)
+                conexion.Open();
+            return conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+            return conexion;
+        }
+
+
+    }
+}
---
+++ FrbaOfertas/FrbaOfertas/Login/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using FrbaOfertas.Conexion;
+
+namespace FrbaOfertas
+{
+    public partial class Login : Form
+    {
+        c_Conexion conexion = new c_Conexion();
+
+        public Login()
+        {
+            InitializeComponent();
+            password.PasswordChar = '*';
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Signinbutton_Click(object sender, EventArgs e)
+        {
+            SqlConnection conex = conexion.AbrirConexion();
+
+            SqlCommand procedure = new SqlCommand("sp_validarUsuario", conex);
+            procedure.CommandType = CommandType.StoredProcedure;
+            procedure.Parameters.AddWithValue("@id_ingresado", SqlDbType.NVarChar).Value = usuarioTxt.Text;
+            procedure.Parameters.Add("@contra_ingresada", SqlDbType.NVarChar).Value = password.Text;
+            procedure.Parameters.Add("@retorno", SqlDbType.Int).Direction = System.Data.ParameterDirection.ReturnValue;
+            procedure.ExecuteNonQuery();
+
+            int retorno = (int)procedure.Parameters["@retorno"].Value;
+
+            if (retorno == 1)
+            {//todo bien
+
+                //VentanaMenu menu = new VentanaMenu();
+                this.Hide();
+                MessageBox.Show("Logueo correcto", "FrbaOfertas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //menu.Show();
+            }
+            else if (retorno == 0)
+            { //hay intentos todavia
+                MessageBox.Show("Contraseña incorrecta, pruebe nuevamente", "FrbaOfertas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                password.Clear();
+            }
+            else if (retorno == -1)
+            { //baja logica
+                MessageBox.Show("Se excedió la cantidad de intentos posibles", "FrbaOfertas", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Hide();
+            }
+            else
+            { //no existe usuario
+                MessageBox.Show("No Existe Usuario", "FrbaOfertas", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                usuarioTxt.Clear();
+                password.Clear();
+
+            }
+        }
+
+        private void UsuarioTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
