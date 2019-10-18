f4402c75e26e58015c4be6b3691adc26f17bc264
Julian <jm.ord98@gmail.com>
2019-10-18 20:20:34 -0300

    Cambios en vista cargar credito

--- FrbaOfertas/FrbaOfertas/CragaCredito/CargarCredito.Designer.cs
+++ FrbaOfertas/FrbaOfertas/CragaCredito/CargarCredito.Designer.cs
@@ -28,126 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboFormaPago = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtTarjeta = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtNombreTitular = new System.Windows.Forms.TextBox();
-            this.fechaVencimiento = new System.Windows.Forms.DateTimePicker();
-            this.label4 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.monto = new System.Windows.Forms.NumericUpDown();
             this.btnCargar = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.txtMonto = new System.Windows.Forms.TextBox();
             this.btnSeleccionarCliente = new System.Windows.Forms.Button();
             this.labelUsuario = new System.Windows.Forms.Label();
             this.txtCliente = new System.Windows.Forms.TextBox();
             this.btnInfoCLiente = new System.Windows.Forms.Button();
+            this.btnAgregarTarjeta = new System.Windows.Forms.Button();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.comboTarjeta = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.monto)).BeginInit();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // comboFormaPago
-            // 
-            this.comboFormaPago.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboFormaPago.FormattingEnabled = true;
-            this.comboFormaPago.Location = new System.Drawing.Point(9, 118);
-            this.comboFormaPago.Name = "comboFormaPago";
-            this.comboFormaPago.Size = new System.Drawing.Size(183, 23);
-            this.comboFormaPago.TabIndex = 19;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 100);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 15);
-            this.label1.TabIndex = 20;
-            this.label1.Text = "Forma de pago";
-            // 
-            // txtTarjeta
-            // 
-            this.txtTarjeta.Location = new System.Drawing.Point(9, 41);
-            this.txtTarjeta.Name = "txtTarjeta";
-            this.txtTarjeta.Size = new System.Drawing.Size(183, 20);
-            this.txtTarjeta.TabIndex = 21;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.label2.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(6, 23);
+            this.label2.Location = new System.Drawing.Point(77, 24);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 15);
+            this.label2.Size = new System.Drawing.Size(84, 15);
             this.label2.TabIndex = 22;
-            this.label2.Text = "Numero de tarjeta";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(6, 82);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(109, 15);
-            this.label3.TabIndex = 24;
-            this.label3.Text = "Nombre del titular";
-            // 
-            // txtNombreTitular
-            // 
-            this.txtNombreTitular.Location = new System.Drawing.Point(9, 100);
-            this.txtNombreTitular.Name = "txtNombreTitular";
-            this.txtNombreTitular.Size = new System.Drawing.Size(183, 20);
-            this.txtNombreTitular.TabIndex = 23;
-            // 
-            // fechaVencimiento
-            // 
-            this.fechaVencimiento.CustomFormat = "yyy-MM-dd";
-            this.fechaVencimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fechaVencimiento.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.fechaVencimiento.Location = new System.Drawing.Point(9, 159);
-            this.fechaVencimiento.Margin = new System.Windows.Forms.Padding(2);
-            this.fechaVencimiento.Name = "fechaVencimiento";
-            this.fechaVencimiento.Size = new System.Drawing.Size(183, 19);
-            this.fechaVencimiento.TabIndex = 25;
-            this.fechaVencimiento.Value = new System.DateTime(2019, 10, 25, 0, 0, 0, 0);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label4.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(6, 142);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(126, 15);
-            this.label4.TabIndex = 26;
-            this.label4.Text = "Fecha de vencimiento";
+            this.label2.Text = "Elija su tarjeta";
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.groupBox1.Controls.Add(this.btnCargar);
+            this.groupBox1.Controls.Add(this.monto);
             this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.txtMonto);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.comboFormaPago);
-            this.groupBox1.Location = new System.Drawing.Point(29, 112);
+            this.groupBox1.Location = new System.Drawing.Point(23, 90);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(475, 240);
+            this.groupBox1.Size = new System.Drawing.Size(471, 159);
             this.groupBox1.TabIndex = 27;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos de la Carga";
             // 
+            // monto
+            // 
+            this.monto.DecimalPlaces = 2;
+            this.monto.Location = new System.Drawing.Point(12, 49);
+            this.monto.Maximum = new decimal(new int[] {
+            -1486618625,
+            232830643,
+            0,
+            0});
+            this.monto.Name = "monto";
+            this.monto.Size = new System.Drawing.Size(183, 20);
+            this.monto.TabIndex = 31;
+            this.monto.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
             // btnCargar
             // 
             this.btnCargar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCargar.Location = new System.Drawing.Point(9, 170);
+            this.btnCargar.Location = new System.Drawing.Point(151, 254);
             this.btnCargar.Margin = new System.Windows.Forms.Padding(2);
             this.btnCargar.Name = "btnCargar";
             this.btnCargar.Size = new System.Drawing.Size(183, 36);
@@ -157,15 +98,14 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.comboTarjeta);
+            this.groupBox2.Controls.Add(this.lblNumero);
+            this.groupBox2.Controls.Add(this.btnAgregarTarjeta);
             this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Controls.Add(this.txtNombreTitular);
-            this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Controls.Add(this.txtTarjeta);
-            this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Controls.Add(this.fechaVencimiento);
-            this.groupBox2.Location = new System.Drawing.Point(254, 19);
+            this.groupBox2.Location = new System.Drawing.Point(220, 19);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(200, 195);
+            this.groupBox2.Size = new System.Drawing.Size(234, 122);
             this.groupBox2.TabIndex = 29;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Datos de la tarjeta";
@@ -175,23 +115,16 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.label5.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(6, 42);
+            this.label5.Location = new System.Drawing.Point(9, 30);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(104, 15);
             this.label5.TabIndex = 28;
             this.label5.Text = "Monto (en pesos)";
             // 
-            // txtMonto
-            // 
-            this.txtMonto.Location = new System.Drawing.Point(9, 60);
-            this.txtMonto.Name = "txtMonto";
-            this.txtMonto.Size = new System.Drawing.Size(183, 20);
-            this.txtMonto.TabIndex = 27;
-            // 
             // btnSeleccionarCliente
             // 
             this.btnSeleccionarCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSeleccionarCliente.Location = new System.Drawing.Point(34, 52);
+            this.btnSeleccionarCliente.Location = new System.Drawing.Point(272, 28);
             this.btnSeleccionarCliente.Margin = new System.Windows.Forms.Padding(2);
             this.btnSeleccionarCliente.Name = "btnSeleccionarCliente";
             this.btnSeleccionarCliente.Size = new System.Drawing.Size(81, 24);
@@ -229,7 +162,7 @@
             this.btnInfoCLiente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnInfoCLiente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInfoCLiente.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.btnInfoCLiente.Location = new System.Drawing.Point(29, 66);
+            this.btnInfoCLiente.Location = new System.Drawing.Point(34, 52);
             this.btnInfoCLiente.Margin = new System.Windows.Forms.Padding(2);
             this.btnInfoCLiente.Name = "btnInfoCLiente";
             this.btnInfoCLiente.Size = new System.Drawing.Size(216, 33);
@@ -240,13 +173,59 @@
             this.btnInfoCLiente.UseVisualStyleBackColor = false;
             this.btnInfoCLiente.Click += new System.EventHandler(this.btnInfoCLiente_Click);
             // 
+            // btnAgregarTarjeta
+            // 
+            this.btnAgregarTarjeta.Location = new System.Drawing.Point(98, 92);
+            this.btnAgregarTarjeta.Name = "btnAgregarTarjeta";
+            this.btnAgregarTarjeta.Size = new System.Drawing.Size(103, 23);
+            this.btnAgregarTarjeta.TabIndex = 27;
+            this.btnAgregarTarjeta.Text = "+ Agregar Tarjeta";
+            this.btnAgregarTarjeta.UseVisualStyleBackColor = true;
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(26, 45);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(44, 13);
+            this.lblNumero.TabIndex = 28;
+            this.lblNumero.Text = "Numero";
+            // 
+            // comboTarjeta
+            // 
+            this.comboTarjeta.FormattingEnabled = true;
+            this.comboTarjeta.Location = new System.Drawing.Point(80, 42);
+            this.comboTarjeta.Name = "comboTarjeta";
+            this.comboTarjeta.Size = new System.Drawing.Size(121, 21);
+            this.comboTarjeta.TabIndex = 29;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(42, 63);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 24);
+            this.button1.TabIndex = 108;
+            this.button1.TabStop = false;
+            this.button1.Text = "Ver informacion de la tarjeta";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // CargarCredito
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(536, 364);
+            this.ClientSize = new System.Drawing.Size(499, 308);
             this.Controls.Add(this.btnSeleccionarCliente);
+            this.Controls.Add(this.btnCargar);
             this.Controls.Add(this.labelUsuario);
             this.Controls.Add(this.txtCliente);
             this.Controls.Add(this.btnInfoCLiente);
@@ -255,6 +234,7 @@
             this.Text = "CargarCredito";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.monto)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
@@ -264,22 +244,19 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboFormaPago;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtTarjeta;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtNombreTitular;
-        private System.Windows.Forms.DateTimePicker fechaVencimiento;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox txtMonto;
         private System.Windows.Forms.Button btnCargar;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button btnSeleccionarCliente;
         private System.Windows.Forms.Label labelUsuario;
         private System.Windows.Forms.TextBox txtCliente;
         private System.Windows.Forms.Button btnInfoCLiente;
+        private System.Windows.Forms.NumericUpDown monto;
+        private System.Windows.Forms.Button btnAgregarTarjeta;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboTarjeta;
+        private System.Windows.Forms.Label lblNumero;
     }
 }
